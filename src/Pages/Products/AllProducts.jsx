b905485e9c1c073912f059071c68e5e44bc9878f
MdFarhan681 <ishrakemon681@gmail.com>
2025-12-06 00:09:33 +0600

    filter

--- src/Pages/Products/AllProducts.jsx
+++ src/Pages/Products/AllProducts.jsx
@@ -16,6 +16,30 @@ const AllProducts = () => {
        <main className="care py-15 flex flex-col w-fit mx-auto ">
         <h2 className="text-secondary text-xl md:text-3xl font-bold text-center pb-5">Our All Products
         </h2>
+
+<div className="filter my-4 w-fit mx-auto shadow-lg flex  ">
+
+
+
+  
+<div className=""><select className="select select-primary w-fit rounded " onChange={(e) => setSort(e.target.value)}>
+<option value="">Price</option>
+<option value="price_asc">Low to High</option>
+<option value="price_desc">High to Low</option>
+</select></div>
+
+  <div className="border border-primary rounded  join-item w-fit ">
+   <button className="btn" >Apply</button>
+  </div>
+
+
+</div>
+        
+
+
+
+
+       
       
       <div className="w-full mycard px-0 grid grid-cols-1 md:grid-cols-4 gap-5  mx-auto place-items-center">
         {
